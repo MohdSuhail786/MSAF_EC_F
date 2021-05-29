6168d2c51d6ddb85aaad35ae0c695b230f21462c
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-05-29 17:24:37 +0530

    updated marquee

--- src/AuthComponents/MainAuth.jsx
+++ src/AuthComponents/MainAuth.jsx
@@ -1,31 +1,62 @@
-import React ,{useState} from 'react';
-import Login from './Login';
-import Register from './Register';
-import Panel from './Panel';
-import './Auth.css';
-import SnackBarComponent from '../CommonComponents/SnackBarComponent';
-
+import React, { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
+import Panel from "./Panel";
+import "./Auth.css";
+import SnackBarComponent from "../CommonComponents/SnackBarComponent";
 
 const MainAuth = (props) => {
-    const [value,setValue] = useState("container")
-    const [open,setOpen] = useState(false)
-    const [message,setMessage] = useState("")
-    const [severity,setSeverity] = useState("success")
-    
-    return (
-        <div className={value}>
-             <marquee style={{float:"right"}} width = "50%">Login Credentials for admin = Email: admin@gmail.com Password: admin </marquee> <br></br>
-             <marquee style={{float:"right"}} width = "50%">Login Credentials for user = Email: user@gmail.com Password: test </marquee>
-            <div className="forms-container">
-                <div className="signin-signup">
-                    <Login callback={()=>props.callback()} setOpen = {(e)=>{setOpen(e)}} setMessage={(e) => {setMessage(e); console.log(e)}} setSeverity = {(e) => setSeverity(e)}/>
-                    <Register onChange = {(e) => {setValue(e)}} setOpen = {(e)=>{setOpen(e)}} setMessage={(e) => {setMessage(e); console.log(e)}} setSeverity = {(e) => setSeverity(e)}/>
-                </div>
-            </div>
-            <Panel onChange = {(e) => {setValue(e)}}/>
-            { open && <SnackBarComponent setOpen = {(e) => {setOpen(false)}} message={message} severity={severity}/> }
-        </div>
-    )
-}
+  const [value, setValue] = useState("container");
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("success");
 
-export default  MainAuth;
+  return (
+    <div className={value}>
+      <div className="forms-container">
+        <div className="signin-signup">
+          <Login
+            callback={() => props.callback()}
+            setOpen={(e) => {
+              setOpen(e);
+            }}
+            setMessage={(e) => {
+              setMessage(e);
+              console.log(e);
+            }}
+            setSeverity={(e) => setSeverity(e)}
+          />
+          <Register
+            onChange={(e) => {
+              setValue(e);
+            }}
+            setOpen={(e) => {
+              setOpen(e);
+            }}
+            setMessage={(e) => {
+              setMessage(e);
+              console.log(e);
+            }}
+            setSeverity={(e) => setSeverity(e)}
+          />
+        </div>
+      </div>
+      <Panel
+        onChange={(e) => {
+          setValue(e);
+        }}
+      />
+      {open && (
+        <SnackBarComponent
+          setOpen={(e) => {
+            setOpen(false);
+          }}
+          message={message}
+          severity={severity}
+        />
+      )}
+    </div>
+  );
+};
+
+export default MainAuth;
